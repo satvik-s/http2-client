71f83e85cbbe105859948279260ec47121d1f853
Satvik Shukla <satvik.shukla@outlook.com>
2021-07-05 12:41:35 -0700

    update exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { Http2Client } from './client';
+export { BufferPayload, JsonPayload, StringPayload } from './types/payloads';
+export { PayloadType } from './types/payloadType';
 export { Http2RequestOptions } from './types/requestOptions';
 export { Http2Response } from './types/response';
